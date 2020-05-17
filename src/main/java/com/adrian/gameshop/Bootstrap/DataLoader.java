8555fa6a96c91071360861760f1cac5bcabf348b
skillexe10 <skillexe10@o2.pl>
2020-05-17 13:41:31 +0200

    Company logos and game images layout

--- src/main/java/com/adrian/gameshop/Bootstrap/DataLoader.java
+++ src/main/java/com/adrian/gameshop/Bootstrap/DataLoader.java
@@ -7,10 +7,18 @@ import com.adrian.gameshop.models.Platform;
 import com.adrian.gameshop.repositories.CategoryRepository;
 import com.adrian.gameshop.repositories.CompanyRepository;
 import com.adrian.gameshop.repositories.GameRepository;
+import com.adrian.gameshop.services.CompanyService;
+import com.adrian.gameshop.services.GameService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
 
 @Slf4j
@@ -20,11 +28,15 @@ public class DataLoader implements CommandLineRunner {
     private final GameRepository gameRepository;
     private final CategoryRepository categoryRepository;
     private final CompanyRepository companyRepository;
+    private final GameService gameService;
+    private final CompanyService companyService;
 
-    public DataLoader(GameRepository gameRepository, CategoryRepository categoryRepository, CompanyRepository companyRepository) {
+    public DataLoader(GameRepository gameRepository, CategoryRepository categoryRepository, CompanyRepository companyRepository, GameService gameService, CompanyService companyService) {
         this.gameRepository = gameRepository;
         this.categoryRepository = categoryRepository;
         this.companyRepository = companyRepository;
+        this.gameService = gameService;
+        this.companyService = companyService;
     }
 
 
@@ -34,7 +46,7 @@ public class DataLoader implements CommandLineRunner {
         loadData();
     }
 
-    private void loadData(){
+    private void loadData() {
 
         // LOAD CATEGORIES
 
@@ -104,6 +116,76 @@ public class DataLoader implements CommandLineRunner {
 
         log.info("####  Loaded " + gameRepository.count() + " games  ####");
 
+        loadImages();
+
+        log.info("####  Loaded images  ####");
+
+        loadLogos();
+
+        log.info("####  Loaded logos  ####");
+    }
+
+    private void loadImages() {
+
+        File witcherFile;
+        File acFile;
+        File farCryFile;
+        MultipartFile witcherImage = null;
+        MultipartFile acImage = null;
+        MultipartFile farCryImage = null;
+
+        try {
+
+            witcherFile = new ClassPathResource("static/images/wiedzmin3_cover.jpg").getFile();
+            witcherImage = new MockMultipartFile("wiedzmin3_cover.jpg", new FileInputStream(witcherFile));
+
+            acFile = new ClassPathResource("static/images/ac_origins_cover.png").getFile();
+            acImage = new MockMultipartFile("ac_origins_cover.jpg",new FileInputStream(acFile));
+
+            farCryFile = new ClassPathResource("static/images/far_cry_3_cover.jpg").getFile();
+            farCryImage = new MockMultipartFile("far_cry_3_cover.jpg",new FileInputStream(farCryFile));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        gameService.saveImageFile(1L, witcherImage);
+        gameService.saveImageFile(2L, acImage);
+        gameService.saveImageFile(3L, farCryImage);
+    }
+
+    private void loadLogos(){
+
+        File cdpFile;
+        File eaFile;
+        File ubisoftFile;
+        File activisionBlizzardFile;
+        MultipartFile cdpLogo = null;
+        MultipartFile eaLogo = null;
+        MultipartFile ubisoftLogo = null;
+        MultipartFile activisionBlizzardLogo = null;
+
+        try {
+
+            cdpFile = new ClassPathResource("static/images/cdp_logo.png").getFile();
+            cdpLogo = new MockMultipartFile("cdp_logo.png", new FileInputStream(cdpFile));
+
+            eaFile = new ClassPathResource("static/images/ea_logo.png").getFile();
+            eaLogo = new MockMultipartFile("ea_logo.png", new FileInputStream(eaFile));
+
+            ubisoftFile = new ClassPathResource("static/images/ubisoft_logo.png").getFile();
+            ubisoftLogo = new MockMultipartFile("ubisoft_logo.png", new FileInputStream(ubisoftFile));
+
+            activisionBlizzardFile = new ClassPathResource("static/images/activision_blizzard_logo.png").getFile();
+            activisionBlizzardLogo = new MockMultipartFile("activision_blizzard_logo.png", new FileInputStream(activisionBlizzardFile));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        companyService.saveImageFile(1L, cdpLogo);
+        companyService.saveImageFile(2L, eaLogo);
+        companyService.saveImageFile(3L, ubisoftLogo);
+        companyService.saveImageFile(4L, activisionBlizzardLogo);
     }
 
 }
