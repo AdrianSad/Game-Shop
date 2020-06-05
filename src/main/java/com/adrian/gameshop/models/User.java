a81fbb1e4afbc05fcbcb6e34443b6451432f465c
skillexe10 <skillexe10@o2.pl>
2020-06-05 19:29:37 +0200

    User model impl

--- src/main/java/com/adrian/gameshop/models/User.java
+++ src/main/java/com/adrian/gameshop/models/User.java
@@ -1,11 +1,16 @@
 package com.adrian.gameshop.models;
 
+import com.adrian.gameshop.validators.ValidEmail;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -20,15 +25,30 @@ public class User {
     @Column(name = "id")
     private Long id;
 
+    @NotNull
+    @NotEmpty
     @Column(name = "firstName")
     private String firstName;
 
+    @NotNull
+    @NotEmpty
     @Column(name = "lastName")
     private String lastName;
 
+    @ValidEmail
+    @NotNull
+    @NotEmpty
     @Column(name = "email")
     private String email;
 
+    @NotNull
+    @NotEmpty
     @Column(name = "password")
     private String password;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "users_roles",
+            joinColumns = @JoinColumn(name = "userID"),
+            inverseJoinColumns = @JoinColumn(name = "roleID"))
+    private Set<UserRole> userRoles = new HashSet<>();
 }
---
+++ src/main/java/com/adrian/gameshop/models/UserRole.java
@@ -0,0 +1,9 @@
+package com.adrian.gameshop.models;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "userRoles")
+public class UserRole extends BaseEntity{
+}
