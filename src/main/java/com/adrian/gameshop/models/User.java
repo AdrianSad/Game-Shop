982b62ecbf994a6836cb2c1e58ee46eb31981548
skillexe10 <skillexe10@o2.pl>
2020-06-05 15:24:02 +0200

    User model impl

---
+++ src/main/java/com/adrian/gameshop/models/User.java
@@ -0,0 +1,34 @@
+package com.adrian.gameshop.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
